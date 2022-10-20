574e14cbb1ce808dfc0f66ee57bbc067faf1b409
Shennie <shengfeng.wu.0308@gmail.com>
2022-10-20 12:19:16 -0400

    Add new feature to JavaScript script

--- script.js
+++ script.js
@@ -5,4 +5,16 @@ const object = {
 
 function fuck(x) => {
   alert('fuck you ' + x);
-} 
+}
+
+// from below this is an experimental class
+class Fucker  {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  get identity() {
+    return {name: this.name, age: this.age}
+  } 
+}
